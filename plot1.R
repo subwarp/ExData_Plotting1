b1c6ed0a1d1d26f59ea24d1cd0e568c43b8b6d07
Carlos <carlos@tikaldigital.com>
2015-08-07 03:46:31 -0700

    work in progress. experimenting

--- plot1.R
+++ plot1.R
@@ -12,19 +12,46 @@ basicConfig(level = 'DEBUG')
 data.file <- file.path("data", "household_power_consumption.txt")
 
 pcdata <- read.table(textConnection(grep("2/1/2007|2/2/2007",
-                                           readLines(data.file)),
-                                    ), sep=";")
+                                         readLines(data.file),
+                                         value = TRUE),
+                                    ), sep=";", na.strings = "?")
+
+colnames(pcdata) <- c("Date", "Time", "Global_active_power", "Global_reactive_power",
+                      "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2",
+                      "Sub_metering_3")
+
+pcdata$Date2 <- lapply(pcdata$Date, as.Date, format = "")
 
 
-x <- rnorm(100)
-y <- x^2
-plot(x,y)
+FixDateString <- function(date.string) {
+  #loginfo("FixDateString - Input: %s (%s)", date.string, class(date.string))
+  if (is.factor(date.string)) {
+    date.string <- as.character(date.string)
+  }
+  date.elements <- unlist(strsplit(date.string, "/"))
+  month <- as.numeric(date.elements[2]);
+  day <- as.numeric(date.elements[2]);  
+  if (month < 10) {
+    date.elements[1] = sprintf("0%s", month)
+  }
+  if (day < 10) {
+    date.elements[2] = sprintf("0%s", day)
+  }  
+  sprintf("%s/%s/%s", date.elements[1], date.elements[2], date.elements[3])     
+}
 
-library(datasets)
 
-hist(airquality$Ozone, nain = "Foo Bar")
+### mapply(function(d, t) {
+###   d <- FixDateString(as.character(d))
+###   date.time <- paste(d, t)
+###   strptime(date.time, "%m/%d/%Y %H:%M:%S")[1][1]
+### }, head(pcdata$Date, 2), head(pcdata$Time, 2), SIMPLIFY = FALSE))
+### 
+### 
+### pcdata <- mutate(pcdata, Date2 = paste(FixDateString(Date), Time))
+### 
+### strptime(paste(FixDateString(Date), Time), "%m/%d/%Y %H:%M:%S"))
+### pcdata <- select(pcdata, -Date2)
 
-par(col = "tomato")
-?colors()
 
-colors()
+hist(pcdata$Global_active_power)
