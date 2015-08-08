754456ea250cbb1487dab3c21a9a726309637319
Carlos <carlos@tikaldigital.com>
2015-08-08 04:25:35 -0700

    Check point. Working version of Plot1

--- plot1.R
+++ plot1.R
@@ -2,39 +2,37 @@
 # Course Assignment 1. Plot 1.
 
 library(dplyr)
-library(logging)
-basicConfig(level = 'DEBUG')
 
-# CONFIG #
+# Running 
 # Note: Set your working directory accordingly.
-# Step 1. Set path to the household_power_consumption.txt
+# Step 1. Load the data via function LoadData(<path.to.data.file>)
+#         ie. pcdata  <- LoadData(file.path("data", "household_power_consumption.txt"))
+# Step 2. Generate graph via function DoPlot1(pcdata)
 
-DoPlot1 <- function(pcdata.file) {
 
-  if (!file.exists(pcdata.file)) {
-    stop(sprintf("Data file %s does not exist. Am out!", pcdata.file))
+LoadData <- function(data.file) {
+  # Loads household power consumption data for 0[1-2]/02/2007
+  # Adds a column Date.Time which is Date Time object.
+  if (!file.exists(data.file)) {
+    stop(sprintf("Data file %s does not exist. Am out!", data.file))
   }
-
-  logdebug("Loading data file: %s", pcdata.file)
-  pcdata <- read.table(textConnection(grep("^2/1/2007|^2/2/2007",
-                                           readLines(pcdata.file),
+  pcdata <- read.table(textConnection(grep("^1/2/2007|^2/2/2007",
+                                           readLines(data.file),
                                            value = TRUE)), sep=";",
                                       na.strings = "?")
-
   colnames(pcdata) <- c("Date", "Time", "Global_active_power",
                         "Global_reactive_power", "Voltage", "Global_intensity",
                         "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
 
+  mutate(pcdata,
+         Date.Time = as.POSIXct(paste(Date, Time),
+             format = "%d/%m/%Y %H:%M:%S"))
+  
+}
 
+DoPlot1 <- function(pcdata) {
   png(file = "plot1.png")
   hist(pcdata$Global_active_power, col="red", main="Global Active Power",
        xlab = "GLobal Active Power (Kilowatts)")
   dev.off()
-}
-
-Test <- function() {
-  # Runs DoPlot1.
-  # Data is in a subdirectory in file: household_power_consumption.txt
-  data.file <- file.path("data", "household_power_consumption.txt")
-  DoPlot1(data.file)
 }
